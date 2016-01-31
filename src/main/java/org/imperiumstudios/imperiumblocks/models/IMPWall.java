16d4d4f5ff87edb0027f91149a97cf0b951b60c0
Kevin Olinger <mail@kevinolinger.net>
2016-01-31 16:51:20 +0100

    Can now place torch on wall & connect with gate

--- src/main/java/org/imperiumstudios/imperiumblocks/models/IMPWall.java
+++ src/main/java/org/imperiumstudios/imperiumblocks/models/IMPWall.java
@@ -35,6 +35,8 @@ import org.imperiumstudios.imperiumblocks.ImperiumBlocks;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockFenceGate;
 import net.minecraft.block.BlockWall;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
@@ -42,6 +44,8 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 public class IMPWall extends BlockWall {
 
 	ImperiumBlocks Core;
@@ -84,6 +88,17 @@ public class IMPWall extends BlockWall {
 	@Override
 	public IIcon getIcon(int side, int metadata) {
 	    return blockIcon;
+	}
+	
+	@Override
+	public boolean canConnectWallTo(IBlockAccess blockAccess, int x, int y, int z) {
+		Block block = blockAccess.getBlock(x, y, z);
+		boolean su = super.canConnectWallTo(blockAccess, x, y, z);
+		return block instanceof BlockFenceGate || su;
+	}
+	
+    public boolean canPlaceTorchOnTop(World world, int x, int y, int z) {
+		return true;
 	}
 }
 
